2e5da051f0f1788cc9c03c18cc7a674ddd63452c
nazandrey <nazandrbox@yandex.ru>
2021-07-24 16:08:23 +0300

    load players at start

---
+++ Assets/My Assets/Scripts/Core/MenuInstaller.cs
@@ -0,0 +1,16 @@
+using NeuroDerby.MenuLogic;
+using NeuroDerby.Players;
+using Zenject;
+
+namespace NeuroDerby.Core
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<EntryPoint>().AsSingle();
+            Container.BindInterfacesTo<PlayersLoader>().AsCached();
+            Container.BindInterfacesTo<PlayersDtoLoader>().AsCached();
+        }
+    }
+}
---
+++ Assets/My Assets/Scripts/MenuLogic/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using NeuroDerby.Players;
+using Zenject;
+
+namespace NeuroDerby.MenuLogic
+{
+    public class EntryPoint : IInitializable
+    {
+        private IPlayersLoader _playersLoader;
+
+        public EntryPoint(IPlayersLoader playersLoader)
+        {
+            _playersLoader = playersLoader;
+        }
+
+        public void Initialize()
+        {
+            _playersLoader.Load();
+        }
+    }
+}
