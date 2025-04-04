a7ed760753bc97b91f0050751040b22923f22a69
nazandrey <nazandrbox@yandex.ru>
2025-04-04 11:48:20 +0300

    add damage to bullet (to die from 5 bullets atm)

--- Assets/My Assets/Scripts/Game/DifficultyConfig.cs
+++ Assets/My Assets/Scripts/Game/DifficultyConfig.cs
@@ -6,7 +6,7 @@ namespace NeuroDerby.Game
     public class DifficultyConfig : ScriptableObject
     {
         [SerializeField][Range(0,2)] private float bulletSpeed;
-        [SerializeField][Range(1,20)] private float bulletDamage;
+        [SerializeField][Range(1,50)] private float bulletDamage;
 
         public float BulletSpeed => bulletSpeed;
         public float BulletDamage => bulletDamage;
